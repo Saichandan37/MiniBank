539e2e27f3002148f78d152680d579f44fe79168
Sai Chandan Singh <142994438+Saichandan37@users.noreply.github.com>
2025-12-24 15:58:34 +0530

    home page

---
+++ src/main/java/com/NewGen/MiniBank/controller/mainController.java
@@ -0,0 +1,13 @@
+package com.NewGen.MiniBank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class mainController {
+
+    @GetMapping("home")
+    public String home(){
+        return "Home page";
+    }
+}
